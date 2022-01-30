56b6372372a2633e19f09cf107051b903f76ff2b
thommodin <thommygalindo@gmail.com>
2022-01-30 18:35:25 +1030

    Fix .csv scraping, df clean up
    Some of the found urls get a raw text respnose, so we need to sort by url suffix (i.e. '.zip' | '.csv') to either unzip or just write to file.
    
    The df was in a weird order, so we convert the 'VALIDATION_DATE' column using pd.to_datetime() and sort by the new date format, newest to oldest.

--- import.py
+++ import.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from tqdm import trange
 import os, sys
 
-def download(urls=None, refresh=False):
+def download(refresh=False):
 
     # requirements
     import requests, zipfile, io, os
@@ -11,40 +12,49 @@ def download(urls=None, refresh=False):
     if not refresh:
         return
 
-    # if no specific urls are supplied scrape the website for all available files
-    if not urls:
+    # scrape thge website for all available files
+    from lxml import html
+    import requests
 
-        from lxml import html
-        import requests
+    # get the page
+    page = requests.get('https://data.sa.gov.au/data/dataset/adelaide-metrocard-validations')
 
-        # get the page
-        page = requests.get('https://data.sa.gov.au/data/dataset/adelaide-metrocard-validations')
+    # extract into tree
+    tree = html.fromstring(page.content)
 
-        # extract into tree
-        tree = html.fromstring(page.content)
+    # get the urls
+    urls = tree.xpath('//a[@target="_blank"]/@href')
 
-        # get the urls
-        urls = tree.xpath('//a[@target="_blank"]/@href')
+    # extract only urls that are datasets
+    urls = list(filter(lambda url: url[:36] == 'https://data.sa.gov.au/data/dataset/', urls))
 
-        # extract only urls that are datasets
-        urls = list(filter(lambda url: url[:36] == 'https://data.sa.gov.au/data/dataset/', urls))
-
-        # extract only the links that have string '/download/bandedvalidations20' in them
-        urls = list(filter(lambda url: '/download/bandedvalidations20' in url, urls))
+    # extract only the links that have string '/download/bandedvalidations20' in them
+    urls = list(filter(lambda url: '/download/bandedvalidations20' in url, urls))
 
     # download and extract the urls
-    for url in urls:
+    print('Downloading files from found download urls!')
+    for i in trange(len(urls)):
 
         # get zip
-        r = requests.get(url)
+        r = requests.get(urls[i])
 
-        # encode as zipfile
-        z = zipfile.ZipFile(io.BytesIO(r.content))
+        # if the url gets a .zip, extract the file
+        if urls[i][-4:] == '.zip':
 
-        # extract
-        z.extractall('data')
+            # encode as zipfile
+            z = zipfile.ZipFile(io.BytesIO(r.content))
 
-def load_data(csvs=None):
+            # extract
+            z.extractall('data')
+
+        # if the url gets a .csv, save it
+        elif urls[i][-4:] == '.csv':
+
+            # write with the last 34 characters as the file name
+            with open(os.path.join('data', urls[i][-34:]), 'w') as f:
+                f.write(r.text)
+
+def load(csvs=None):
 
     # construct paths for data files if not supplied, platform independent
     if not csvs:
@@ -63,15 +73,22 @@ def load_data(csvs=None):
     if len(csvs) < 1: sys.exit('No data in data folder!')
 
     # platform independent read all csv in 'data' folder
-    return pd.concat([pd.read_csv(csv) for csv in csvs])
+    df = pd.concat([pd.read_csv(csv) for csv in csvs])
+
+    # reorder by date, newest to oldest
+    df['VALIDATION_DATE'] = pd.to_datetime(df['VALIDATION_DATE'])
+    df.sort_values(ascending=False, by='VALIDATION_DATE', inplace=True)
+
+    return df.reset_index(drop=True)
+
 
 def main():
 
     # download all the data
-    download(refresh=True)
+    download(refresh=refresh)
 
     # construct list of data directories
-    df = load_data()
+    df = load()
 
     print(df)
 
